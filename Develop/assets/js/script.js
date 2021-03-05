d554987bb50c6d59669e57309fdef8cc1face484
Tim Weyel <tweyel@gmail.com>
2021-03-05 14:35:04 -0800

    save hour and description to localStorage and it worksgit add .! yahoo!

--- Develop/assets/js/script.js
+++ Develop/assets/js/script.js
@@ -3,4 +3,16 @@ $( document ).ready(function() {
     //display the current date and time
     var todaysDate = moment().format('dddd, MMM Do YYYY, HH:mm a');
     $("#currentDay").text(todaysDate);
+
+    //save button events
+    $(".saveBtn").on("click", function() {
+      //on click, get the description text and the hour tied to the text so it can be put in localStorage
+      var descText = $(this).siblings(".description").val();
+      var hour = $(this).parent().attr("id");
+
+      //put descText and hour in localStorage
+      localStorage.setItem(hour, descText);
+    });
+
+
 });
