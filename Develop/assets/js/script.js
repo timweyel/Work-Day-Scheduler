0dd91b6a816976e0a40be3c03f18f7c189c01918
Tim Weyel <tweyel@gmail.com>
2021-03-05 14:41:26 -0800

    get key-value pairs from localStorage

--- Develop/assets/js/script.js
+++ Develop/assets/js/script.js
@@ -14,5 +14,15 @@ $( document ).ready(function() {
       localStorage.setItem(hour, descText);
     });
 
+    $("#8am .description").val(localStorage.getItem("8am"));
+    $("#9am .description").val(localStorage.getItem("9am"));
+    $("#10am .description").val(localStorage.getItem("10am"));
+    $("#11am .description").val(localStorage.getItem("11am"));
+    $("#12pm .description").val(localStorage.getItem("12pm"));
+    $("#1pm .description").val(localStorage.getItem("1pm"));
+    $("#2pm .description").val(localStorage.getItem("2pm"));
+    $("#3pm .description").val(localStorage.getItem("3pm"));
+    $("#4pm .description").val(localStorage.getItem("4pm"));
+    $("#5pm .description").val(localStorage.getItem("5pm"));
 
 });
