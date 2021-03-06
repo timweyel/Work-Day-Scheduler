b36e3928fa1d5b2ea71dddb5e36f3d3d75e60ac4
Tim Weyel <tweyel@gmail.com>
2021-03-05 18:44:19 -0800

    timeblock colors added. not updating corrctly according to time. see TODO, line 41 in js.

--- Develop/assets/js/script.js
+++ Develop/assets/js/script.js
@@ -1,7 +1,7 @@
 
 $( document ).ready(function() {
     //display the current date and time
-    var todaysDate = moment().format('dddd, MMM Do YYYY, HH:mm a');
+    var todaysDate = moment().format('dddd, MMMM Do YYYY, h:mm:ss a');
     $("#currentDay").text(todaysDate);
 
     //save button events
@@ -14,15 +14,50 @@ $( document ).ready(function() {
       localStorage.setItem(hour, descText);
     });
 
-    $("#8am .description").val(localStorage.getItem("8am"));
-    $("#9am .description").val(localStorage.getItem("9am"));
-    $("#10am .description").val(localStorage.getItem("10am"));
-    $("#11am .description").val(localStorage.getItem("11am"));
-    $("#12pm .description").val(localStorage.getItem("12pm"));
-    $("#1pm .description").val(localStorage.getItem("1pm"));
-    $("#2pm .description").val(localStorage.getItem("2pm"));
-    $("#3pm .description").val(localStorage.getItem("3pm"));
-    $("#4pm .description").val(localStorage.getItem("4pm"));
-    $("#5pm .description").val(localStorage.getItem("5pm"));
+    $("#8 .description").val(localStorage.getItem("8am"));
+    $("#9 .description").val(localStorage.getItem("9am"));
+    $("#10 .description").val(localStorage.getItem("10am"));
+    $("#11 .description").val(localStorage.getItem("11am"));
+    $("#12 .description").val(localStorage.getItem("12pm"));
+    $("#1 .description").val(localStorage.getItem("1pm"));
+    $("#2 .description").val(localStorage.getItem("2pm"));
+    $("#3 .description").val(localStorage.getItem("3pm"));
+    $("#4 .description").val(localStorage.getItem("4pm"));
+    $("#5 .description").val(localStorage.getItem("5pm"));
 
+
+  //get the current time
+  //if timeblock < current time(hour?), then past
+  //if timeblock = current hour, then present
+  //if timeblock > current hour, then future
+  //then restart the process (setInterval?)
+
+  function checkTime() {
+    //what hour of the day is it?
+    var currentTime = moment().hours();
+    // var currentTime = '12';
+
+    //check the current time and depending on >, =, or <, add/remove class for background colors
+    $(".timeblock").each(function() {
+      var hourRow = $(this).attr("id");
+
+      if (hourRow < currentTime) {
+        $(this).removeClass("future");
+        $(this).removeClass("present");
+        $(this).addClass("past");
+      }
+      else if (hourRow === currentTime) {
+        $(this).removeClass("past");
+        $(this).removeClass("future");
+        $(this).addClass("present");
+      }
+      else {
+        $(this).removeClass("present");
+        $(this).removeClass("past");
+        $(this).addClass("future");
+      }
+    })
+  }
+
+  checkTime();
 });
